484dac25c31d36707d30df8c08d804a37d33d55a
송유현 <doong3373@gmail.com>
2021-07-12 21:00:23 +0900

    [refactor] move dto로 인한  경로 변경

--- nest-auth/src/users/users.controller.ts
+++ nest-auth/src/users/users.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
-import { CreateUserDto } from '../../dto/create-user.dto';
+import { CreateUserDto } from '../dto/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
