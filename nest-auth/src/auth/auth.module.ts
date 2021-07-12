2498905bb9e2d083354ad3e0c0ad729ade866383
송유현 <doong3373@gmail.com>
2021-07-12 17:16:44 +0900

    [ADD] API End Point(URL)에 대한 호출에 사용될 JWT를 반환 및 반환된 JWT로 인증 성공

--- nest-auth/src/auth/auth.module.ts
+++ nest-auth/src/auth/auth.module.ts
@@ -3,9 +3,26 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthController } from './auth.controller';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '60s' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
--- nest-auth/src/auth/auth.service.ts
+++ nest-auth/src/auth/auth.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/entity/users.entity';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UsersService) {}
+    constructor(
+        private userService: UsersService,
+        private jwtService: JwtService,
+    ) {}
 
-    async validateUser(email: string, pass: string): Promise<any> {
-        const user = await this.userService.find(email);
-        if(user && user.validatePassword(pass)) {
+    async validateUser(email: string, password: string): Promise<any> {
+        const user = await this.userService.findByEmail(email);
+        if(user && user.validatePassword(password)) {
             const { password, ...result } = user;
             return result;
         }
         return null;
+    }
+    
+    async login(user: User) {
+        const payload = { userId: user.id };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
     }
 }
