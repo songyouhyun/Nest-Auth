dd8f64b89cc9880c404ad00ba3f5c24217b71055
송유현 <doong3373@gmail.com>
2021-07-12 20:14:43 +0900

    [modified] payload claim

--- nest-auth/src/auth/auth.service.ts
+++ nest-auth/src/auth/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
     }
     
     async login(user: User) {
-        const payload = { userId: user.id };
+        const payload = { userId: user.id, isActive: user.isActive };
         return {
             access_token: this.jwtService.sign(payload),
         };
