2b439f3ca22d557ec63e944cc412f3ed389b1a83
송유현 <doong3373@gmail.com>
2021-07-14 10:11:11 +0900

    [refactor] 필요없는 import 삭제

--- nest-auth/src/app.controller.ts
+++ nest-auth/src/app.controller.ts
@@ -1,10 +1,8 @@
 import { Controller, Get, UseGuards, Request } from '@nestjs/common';
-import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
